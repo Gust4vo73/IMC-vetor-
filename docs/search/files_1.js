da71e7f75878d3412888ddf5c7d0091fe979087d
Gust4vo73 <151526988+Gust4vo73@users.noreply.github.com>
2023-12-06 09:53:10 -0300

    Add files via upload

---
+++ docs/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['vetor_20ordenado_2ec_0',['vetor ordenado.c',['../vetor_01ordenado_8c.html',1,'']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../_i_m_c_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main(void):&#160;IMC.c'],['../vetor_01ordenado_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;vetor ordenado.c']]]
+];
